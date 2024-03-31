2e707b38b9ec3d4f1c86551cc6f1961ab2cedd4d
Code bee <codebee345@outlook.com>
2024-03-31 19:18:09 +0100

    added send contact email feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,155 @@
 const navBar = document.getElementById("nav-bar");
-const mobile = document.querySelector('.mobile')
+const mobile = document.querySelector(".mobile");
+const contactForm = document.getElementById("contact-form");
+let selectedInterest= ''
 
+// transitionModal('contact-modal')
+
+
+function enterContactModalLoadingState(){
+  document.querySelector('.loading-container').classList.add('visible')
+  document.querySelector('.submit-contact-modal-form').disabled = true
+}
+
+function resetContactModalLoadingState(){
+  document.querySelector('.loading-container').classList.remove('visible')
+  document.querySelector('.submit-contact-modal-form').disabled = false
+}
+
+
+function generateRandomString() {
+  return Math.random().toString(36).slice(2);
+}
+
+function sendEmail({
+  toEmail,
+  toName,
+  senderName = "Auspicious website",
+  senderEmail = "onuhudoudo@gmail.com",
+  subject = "Test",
+  htmlContent,
+  onSuccess = ()=>{}, 
+  onError = ()=>{},
+  onFinished = ()=>{}
+}) {
+  if (!toEmail || !toName || !htmlContent) return;
+
+  fetch("https://api.sendinblue.com/v3/smtp/email", {
+    method: "POST",
+    headers: {
+      accept: "application/json",
+      "api-key":
+        "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",
+      "content-type": "application/json",
+      "X-Sib-Sandbox": "drop",
+    },
+    body: JSON.stringify({
+      sender: {
+        name: senderName,
+        email: senderEmail,
+      },
+      to: [
+        {
+          email: toEmail,
+          name: toName,
+        },
+      ],
+      subject: subject,
+      htmlContent: htmlContent,
+      headers: {
+        "X-Mailin-custom":
+          "custom_header_1:custom_value_1|custom_header_2:custom_value_2|custom_header_3:custom_value_3",
+        charset: "iso-8859-1",
+      },
+    }),
+  })
+    .then((response) =>{
+      if(response.ok){
+        onSuccess()
+      }
+      else{
+        onError()
+      }
+      return response.json()
+    })
+    .then((data) => console.log(data))
+    .catch((error) => console.error("Error:", error))
+    .finally(onFinished)
+}
+
+contactForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  if(selectedInterest === '') return
+
+  enterContactModalLoadingState()
+
+  const formData = new FormData(e.target)
+  const name = formData.get('name')
+  const contact = formData.get('contact')
+
+
+  const htmlContent = `
+  <html>
+  <head></head>
+  <body style="padding: 30px;">
+      <br>
+      <h1>New Solar installation request</h1>
+      <br><br>
+
+      <p><b>Name: </b> ${name}</p> <br>
+      <p><b>Contact info: </b> ${contact}</p> <br>
+      <p><b>Reqeusted Item:</b> ${selectedInterest}</p> <br>
+      <br>
+  </body>
+  </html>
+  `
+  
+  sendEmail({
+   toEmail: "onuhudoudo@gmail.com",
+   toName: "Sulaiman Popoola Olasunkianmi",
+   subject: `Solar Installation request from Auspicious website ~id:${generateRandomString()}`,
+   htmlContent: htmlContent,
+   onFinished: ()=>{
+    resetContactModalLoadingState()
+    transitionModal('None')
+   },
+   onSuccess: ()=> {
+    showToast({
+      message: 'Thank you for your inquiry. We have received your request and will be in touch within 1-3 days to discuss it further.',
+      style: 'success',
+      duration: 10000
+    })
+   },
+   onError: ()=> {
+    showToast({
+      message: 'An error occured while sending email, please try again later.',
+      style: 'success',
+      duration: 5000
+    })
+   }
+  })
+});
+
+
+
+document.querySelectorAll(".btn-get-started").forEach(function (btn) {
+  btn.addEventListener("click", function (clicked) {
+   
+    const targetBtn = clicked.target;
+    const planName = targetBtn.getAttribute("data-planname");
+    showContactModal(planName);
+    selectedInterest = planName
+  });
+});
 
 mobile.addEventListener("click", openNav);
-  
-  document.querySelector(".close-nav").addEventListener("click", closeNav);
 
-  function openNav(){
-    navBar.classList.add("visible");
-  }
+document.querySelector(".close-nav").addEventListener("click", closeNav);
 
-  function closeNav(){
-    navBar.classList.remove("visible");
+function openNav() {
+  navBar.classList.add("visible");
+}
 
-  }
+function closeNav() {
+  navBar.classList.remove("visible");
+}
